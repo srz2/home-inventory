f35a7d9a4a98dea1464efd6c6b166d3ba56da74f
srz2 <srz222@gmail.com>
2021-09-26 15:56:57 -0400

    Added reload to header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import {Link} from 'react-router-dom'
 
 const Header = ({toggleAddDialog}) => {
+
+    function reloadToHome() {
+        window.location.reload(false);
+    }
+
     return (
         <div className="header">
-            <h2>Home Inventory</h2>
+            <h2 onClick={reloadToHome}>Home Inventory</h2>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
