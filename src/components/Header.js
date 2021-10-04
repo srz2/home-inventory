2b24d75747d405e191e34cbf6c0ad3a272c3f4ba
srz2 <srz222@gmail.com>
2021-10-04 18:10:00 -0400

    Added more mobile friendly experience

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,8 @@ const Header = ({toggleAddDialog}) => {
         <div className="header">
             <h2 onClick={reloadToHome}>Home Inventory</h2>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+                <li className="hide-mobile"><Link to="/">Home</Link></li>
+                <li className="hide-mobile"><Link to="/settings">Settings</Link></li>
                 <li className="special" onClick={toggleAddDialog}>Add New</li>
             </ul>
         </div>
