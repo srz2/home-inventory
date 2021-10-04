bcd281fe279ee8fcad7c5b322cf100145a0dcb19
srz2 <srz222@gmail.com>
2021-10-04 09:41:23 -0400

    Added dev mode from enviornment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
 import env from 'react-dotenv'
 
+var local_dev;
+if (env.LOCAL_DEV !== undefined && env.LOCAL_DEV === 'true'){
+    local_dev = true;
+} else {
+    local_dev = false;
+}
+
 const config = {
-    LOCAL_DEV: false,
+    LOCAL_DEV: local_dev,
     API_KEY: env.api_key
 }
 
